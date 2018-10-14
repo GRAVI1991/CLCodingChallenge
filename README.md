3c1796991b3e16a812be6e21d955aae7c5c37422
GRAVI1991 <31440492+GRAVI1991@users.noreply.github.com>
2018-10-14 20:33:43 +0530

    nodejs Code for test

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# CLCodingChallenge
+# Coding Challenge
+
+## Requirements:
+
+1.	Write an API endpoint that accepts a GitHub ID and returns Follower GitHub ID’s (up to 5 Followers total) associated with the passed in GitHub ID.  Retrieve data up to 3 levels deep, repeating the process of retrieving Followers (up to 5 Followers total) for each Follower found.  Data should be returned in JSON format.
+2.	Code should be checked into a public GitHub repository, so that the CenturyLink team members can review your code at any time.
+3.	A readme file should be included and checked into GitHub with instructions on how to execute and test your API.
+4.	Bonus credit: API endpoint is publicly accessible and fully functional, so that CenturyLink team members can execute and test your API at any time.
+
+## Bonus Challenge:
+
+The bonus challenge NOT required!
+
+1.	Write an API endpoint that accepts a GitHub ID and retrieves the Repository names (up to 3 Repositories total) associated with the passed in GitHub ID, along with the Stargazer GitHub ID’s (up to 3 Stargazers total) associated with each Repository.  Retrieve data up to 3 levels deep, repeating the process of retrieving the associated Repositories (up to 3 Repositories total) for each Stargazer (up to 3 Stargazers total) found.  Data should be returned in JSON format.
+2.	See requirements 2, 3, and 4 above.
+
+## Tips:
+
+1.	You’ll need a GitHub account (which are free) to complete this coding challenge.
+2.	GitHub provides API’s for retrieving Followers, Repositories, and Stargazers.  Please see the following GitHub API documentation:
+
+	a.	https://developer.github.com/v3/users/followers/
+
+	b.	https://developer.github.com/v3/repos/
+
+	c.	https://developer.github.com/v3/activity/starring/
+
+
+# Pre-reqs
+To build and run this app locally you will need a few things:
+- Install [Node.js](https://nodejs.org/en/)
+- Install [MongoDB](https://docs.mongodb.com/manual/installation/)
+- Install [VS Code](https://code.visualstudio.com/) (you can use any)
+
+
+# Getting started
+- Clone the repository
+```
+git clone <name of repositry>
+```
+- Install dependencies
+```
+cd <project_name>
+npm install
+```
+- Configure your mongoDB server
+```bash
+# create the db directory
+sudo mkdir -p /data/db
+# give the db correct read/write permissions
+sudo chmod 777 /data/db
+```
+- Start your mongoDB server (you'll probably want another command prompt)
+```
+mongod
+```
+- Run the project
+```
+npm start
+```
+
+### Github Followers
+
+To receive the first 5 followers of a github user, 3 levels deep, append either the local url with:
+
+/:username/followers
+
+Where "username" is the github username.
+
+### Github Repos
+
+To receive the first 3 repositories of a github user, and the first 3 stargazers of the repo, append either the local url with:
+
+/:username/repos
+
+Where "username" is the github username.
---
+++ server.js
@@ -0,0 +1,202 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var async = require('async');
+
+app.get('/', function(req, res){
+  res.status(200).send("CenturyLink Coding Challenge");
+});
+
+var firstLevelFollowers = [];
+var secondLevelFollowers = [];
+var thirdLevelFollowers = [];
+var userRepos = [];
+var starGazers = [];
+
+app.get('/:username([a-zA-Z0-9_]+)\/followers', function(req, res){
+
+   async.waterfall ([
+    function getFirstLevelFollowers(callback) {
+      var usersFollowersURL = 'https://api.github.com/users/' + req.params.username + '/followers';
+      var options = {
+        url: usersFollowersURL,
+        headers: {
+          'User-Agent': 'GRAVI1991'
+        }
+      };
+      request.get(options, function (error, response, body) {
+        if (error) {
+          callback(error, null);
+          console.log('statusCode:', response && response.statusCode);
+          return;
+        }
+
+          firstLevelFollowers = [];
+          var jsonObj = JSON.parse(body);
+          for (var i = 0; i < jsonObj.length && i < 5; i++) {
+            firstLevelFollowers.push(jsonObj[i]);
+          }
+
+          callback(null, firstLevelFollowers);
+      });
+    },
+    function getSecondLevelFollowers(firstGithubFollowers, callback) {
+
+        async.each(firstGithubFollowers, function(follower, callback) {
+
+          var usersFollowersURL = 'https://api.github.com/users/' + follower.login + '/followers';
+          var options = {
+            url: usersFollowersURL,
+            headers: {
+              'User-Agent': 'GRAVI1991'
+            }
+          };
+
+          follower.second_level_followers = [];
+          request.get(options, function(error, response, body) {
+              if (error) {
+                callback(error, null);
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                return;
+              }
+
+              secondLevelFollowers = [];
+              var jsonObj = JSON.parse(body);
+              for (var i = 0; i < jsonObj.length && i < 5; i++) {
+                follower.second_level_followers.push(jsonObj[i]);
+                secondLevelFollowers.push(jsonObj[i]);
+              }
+              callback();
+          });
+
+        }, function(err) {
+          if (err) {
+            console.log("Async each failed.");
+          } else {
+            callback(null, secondLevelFollowers);
+          }
+        });
+    },
+    function getThirdLevelFollowers(secondGithubFollowers, callback) {
+        async.each(secondGithubFollowers, function(follower, callback) {
+          var usersFollowersURL = 'https://api.github.com/users/' + follower.login + '/followers';
+          var options = {
+            url: usersFollowersURL,
+            headers: {
+              'User-Agent': 'GRAVI1991'
+            }
+          };
+          follower.third_level_followers = [];
+          request.get(options, function(error, response, body) {
+              if (error) {
+                callback(error, null);
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                return;
+              }
+
+              var jsonObj = JSON.parse(body);
+              for (var i = 0; i < jsonObj.length && i < 5; i++) {
+                follower.third_level_followers.push(jsonObj[i]);
+                thirdLevelFollowers.push(jsonObj[i]);
+              }
+
+              callback();
+          });
+
+        }, function(err) {
+            if (err) {
+              console.log("Async each failed.");
+            } else {
+              res.send(firstLevelFollowers);
+            }
+        });
+    }
+
+    ], function(err, result) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.get('/:username([a-zA-Z0-9_]+)\/repos', function(req, res){
+  async.waterfall ([
+    function getRepos(callback) {
+      var userReposURL = 'https://api.github.com/users/' + req.params.username + '/repos';
+      var options = {
+        url: userReposURL,
+        headers: {
+          'User-Agent': 'GRAVI1991'
+        }
+      };
+
+      request.get(options, function (error, response, body) {
+        if (error) {
+          callback(error, null);
+          console.log('statusCode:', response && response.statusCode);
+          return;
+        }
+
+        userRepos = [];
+        var jsonObj = JSON.parse(body);
+        for (var i = 0; i < jsonObj.length && i < 3; i++) {
+          userRepos.push(jsonObj[i]);
+        }
+
+        // res.send(userRepos);
+        callback(null, userRepos);
+      });
+    },
+
+    function getStarGazers(repos, callback) {
+      async.each(repos, function(repo, callback) {
+
+        var userStarGazersURL = 'https://api.github.com/repos/' + req.params.username + '/' + repo.name + '/stargazers';
+        console.log(userStarGazersURL);
+          var options = {
+            url: userStarGazersURL,
+            headers: {
+              'User-Agent': 'GRAVI1991'
+            }
+          };
+        repo.stargazers = [];
+
+        request.get(options, function(error, response, body) {
+              if (error) {
+                callback(error, null);
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                return;
+              }
+
+              var jsonObj = JSON.parse(body);
+              for (var i = 0; i < jsonObj.length && i < 3; i++) {
+                repo.stargazers.push(jsonObj[i]);
+                starGazers.push(jsonObj[i]);
+              }
+
+              callback();
+        });
+      }, function(err) {
+            if (err) {
+              console.log("Async each failed.");
+            } else {
+              res.send(userRepos);
+            }
+        });
+    }
+  ], function(err, result) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.listen(8080);
